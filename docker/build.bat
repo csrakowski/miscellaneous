382555a3f8bcfbbd113c9c441b4f1fb2be941d67
Christiaan Rakowski <cs.rakowski@gmail.com>
2023-12-17 18:04:36 +0100

    Updated base image to include tzdata

--- docker/build.bat
+++ docker/build.bat
@@ -1,3 +1,4 @@
-docker buildx build --provenance=true --sbom=true -t csrakowski/aspnet:8-alpine --push .
-REM docker build . --file Dockerfile -t csrakowski/aspnet:8-alpine
-REM docker run -it --rm csrakowski/aspnet:8-alpine /bin/ash
+REM docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/aspnet:8-alpine --push .
+REM docker buildx build --platform linux/amd64,linux/arm64 --no-cache -t csrakowski/aspnet:8-alpine-dev .
+docker build .  --no-cache --file Dockerfile -t csrakowski/aspnet:8-alpine-dev
+docker run -it --rm csrakowski/aspnet:8-alpine-dev /bin/ash
