06b3efa6283e1c886298e7fc8abe843977f50634
Christiaan Rakowski <cs.rakowski@gmail.com>
2024-01-10 18:58:40 +0100

    Updated alpine3.19 to mainline release

--- docker/build.bat
+++ docker/build.bat
@@ -1,4 +1,4 @@
-REM docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/aspnet:8-alpine3.19 --push .\aspnet-nightly\
+REM docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/aspnet:8-alpine3.19 --push .\aspnet-alpine3.19\
 REM docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/aspnet:8-alpine --push .\aspnet\
-docker buildx build --platform linux/amd64,linux/arm64 --no-cache -t csrakowski/aspnet:8-alpine3.19-dev .\aspnet-nightly\
+docker buildx build --platform linux/amd64,linux/arm64 --no-cache -t csrakowski/aspnet:8-alpine3.19-dev .\aspnet-alpine3.19\
 docker buildx build --platform linux/amd64,linux/arm64 --no-cache -t csrakowski/aspnet:8-alpine-dev .\aspnet\
