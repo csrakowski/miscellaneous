2ee1d7baeedfa5e1d8849e69ae8c0674ff616719
Christiaan Rakowski <cs.rakowski@gmail.com>
2024-02-24 20:28:50 +0100

    Now that alpine3.19 is the default, we don't need to build that anymore. Instead, now we build .NET 9 preview.

--- docker/build.bat
+++ docker/build.bat
@@ -1,4 +1,4 @@
-REM docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/aspnet:8-alpine3.19 --push .\aspnet-alpine3.19\
-REM docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/aspnet:8-alpine --push .\aspnet\
-docker buildx build --platform linux/amd64,linux/arm64 --no-cache -t csrakowski/aspnet:8-alpine3.19-dev .\aspnet-alpine3.19\
-docker buildx build --platform linux/amd64,linux/arm64 --no-cache -t csrakowski/aspnet:8-alpine-dev .\aspnet\
+REM docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/aspnet:9-alpine --push .\aspnet-9\
+REM docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/aspnet:8-alpine --push .\aspnet-8\
+docker buildx build --platform linux/amd64,linux/arm64 --no-cache -t csrakowski/aspnet:9-alpine-dev .\aspnet-9\
+docker buildx build --platform linux/amd64,linux/arm64 --no-cache -t csrakowski/aspnet:8-alpine-dev .\aspnet-8\
--- docker/test.bat
+++ docker/test.bat
@@ -1,7 +1,7 @@
-REM Mainline Alpine 3.18
-docker build .\aspnet\ --no-cache --file .\aspnet\Dockerfile -t csrakowski/aspnet:8-alpine-dev
+REM Mainline Alpine .NET 8
+docker build .\aspnet-8\ --no-cache --file .\aspnet-8\Dockerfile -t csrakowski/aspnet:8-alpine-dev
 docker run -it --rm csrakowski/aspnet:8-alpine-dev /bin/ash
 
-REM Mainline Alpine 3.19
-docker build .\aspnet-alpine3.19\ --no-cache --file .\aspnet-alpine3.19\Dockerfile -t csrakowski/aspnet:8-alpine3.19-dev
-docker run -it --rm csrakowski/aspnet:8-alpine3.19-dev /bin/ash
+REM Preview Alpine .NET 9
+docker build .\aspnet-9\ --no-cache --file .\aspnet-9\Dockerfile -t csrakowski/aspnet:9-alpine-dev
+docker run -it --rm csrakowski/aspnet:9-alpine-dev /bin/ash
