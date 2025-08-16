ab8d6a3c68aa8b6c6386325f9fe088c66fa64d74
Christiaan Rakowski <cs.rakowski@gmail.com>
2025-08-16 16:35:56 +0200

    Added postgres-cli helper

--- docker/build.bat
+++ docker/build.bat
@@ -1,4 +1,6 @@
 REM docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/aspnet:9-alpine --push .\aspnet-9\
 REM docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/aspnet:8-alpine --push .\aspnet-8\
+REM docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/postgres-cli:latest --push .\postgres-cli\
 docker buildx build --platform linux/amd64,linux/arm64 --no-cache -t csrakowski/aspnet:9-alpine-dev .\aspnet-9\
 docker buildx build --platform linux/amd64,linux/arm64 --no-cache -t csrakowski/aspnet:8-alpine-dev .\aspnet-8\
+docker buildx build --platform linux/amd64,linux/arm64 --no-cache -t csrakowski/postgres-cli:dev .\postgres-cli\
--- docker/test.bat
+++ docker/test.bat
@@ -5,3 +5,7 @@ docker run -it --rm csrakowski/aspnet:8-alpine-dev /bin/ash
 REM Preview Alpine .NET 9
 docker build .\aspnet-9\ --no-cache --file .\aspnet-9\Dockerfile -t csrakowski/aspnet:9-alpine-dev
 docker run -it --rm csrakowski/aspnet:9-alpine-dev /bin/ash
+
+REM Preview Postgres CLI
+docker build .\postgres-cli\ --no-cache --file .\postgres-cli\Dockerfile -t csrakowski/postgres-cli:dev
+docker run -it --rm csrakowski/postgres-cli:dev /bin/ash
