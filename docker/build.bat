77204e36913762bfafa0a4532e1fd7fa5758a4d2
Christiaan Rakowski <cs.rakowski@gmail.com>
2025-11-11 20:39:04 +0100

    Added .NET 10 image

--- docker/build.bat
+++ docker/build.bat
@@ -1,8 +1,10 @@
+docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/aspnet:10-alpine .\aspnet-10\
 docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/aspnet:9-alpine .\aspnet-9\
 docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/aspnet:8-alpine .\aspnet-8\
 docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/postgres-cli:latest .\postgres-cli\
 docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/bitbucket-pipeline-runner:latest .\bitbucket-pipeline-runner\
 
+REM docker push csrakowski/aspnet:10-alpine
 REM docker push csrakowski/aspnet:9-alpine
 REM docker push csrakowski/aspnet:8-alpine
 REM docker push csrakowski/postgres-cli:latest
--- docker/test.bat
+++ docker/test.bat
@@ -6,6 +6,10 @@ REM Preview Alpine .NET 9
 docker build .\aspnet-9\ --no-cache --file .\aspnet-9\Dockerfile -t csrakowski/aspnet:9-alpine-dev
 docker run -it --rm csrakowski/aspnet:9-alpine-dev /bin/ash
 
+REM Preview Alpine .NET 10
+docker build .\aspnet-10\ --no-cache --file .\aspnet-10\Dockerfile -t csrakowski/aspnet:10-alpine-dev
+docker run -it --rm csrakowski/aspnet:10-alpine-dev /bin/ash
+
 REM Preview Postgres CLI
 docker build .\postgres-cli\ --no-cache --file .\postgres-cli\Dockerfile -t csrakowski/postgres-cli:dev
 docker run -it --rm csrakowski/postgres-cli:dev pg_dump  -h postgres.imedia -p 31582 -U postgres
