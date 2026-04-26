b0ef8cae6bb7f332fcc496c9f1ac9145f6375e9e
Christiaan Rakowski <cs.rakowski@gmail.com>
2026-04-26 16:00:40 +0200

    Bumped up the tags again

--- docker/build.bat
+++ docker/build.bat
@@ -5,9 +5,9 @@ docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=t
 docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/bitbucket-pipeline-runner:latest .\bitbucket-pipeline-runner\
 docker buildx build --platform linux/amd64,linux/arm64 --no-cache --provenance=true --sbom=true -t csrakowski/python:latest .\python\
 
-REM docker push csrakowski/aspnet:10-alpine
-REM docker push csrakowski/aspnet:9-alpine
 REM docker push csrakowski/aspnet:8-alpine
+REM docker push csrakowski/aspnet:9-alpine
+REM docker push csrakowski/aspnet:10-alpine
 REM docker push csrakowski/postgres-cli:latest
 REM docker push csrakowski/bitbucket-pipeline-runner:latest
 REM docker push csrakowski/python:latest
